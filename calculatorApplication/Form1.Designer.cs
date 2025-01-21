149898b26fd68882d3051787cfef3e2a032233a0
Aryan Tripathi <133272665+AryanTripathi17@users.noreply.github.com>
2025-01-20 20:04:26 -0500

    Add files via upload

---
+++ calculatorApplication/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace calculatorApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            one = new Button();
+            two = new Button();
+            three = new Button();
+            four = new Button();
+            button5 = new Button();
+            six = new Button();
+            seven = new Button();
+            eight = new Button();
+            nine = new Button();
+            ten = new Button();
+            eleven = new Button();
+            twelve = new Button();
+            thirteen = new Button();
+            fourteen = new Button();
+            fifteen = new Button();
+            sixteen = new Button();
+            button17 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(37, 33);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(722, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // one
+            // 
+            one.BackColor = SystemColors.HotTrack;
+            one.Location = new Point(12, 71);
+            one.Name = "one";
+            one.Size = new Size(189, 62);
+            one.TabIndex = 1;
+            one.Text = "0";
+            one.UseVisualStyleBackColor = false;
+            one.Click += one_Click;
+            // 
+            // two
+            // 
+            two.BackColor = SystemColors.HotTrack;
+            two.Location = new Point(216, 71);
+            two.Name = "two";
+            two.Size = new Size(166, 62);
+            two.TabIndex = 2;
+            two.Text = "1\r\n";
+            two.UseVisualStyleBackColor = false;
+            two.Click += two_Click;
+            // 
+            // three
+            // 
+            three.BackColor = SystemColors.HotTrack;
+            three.Location = new Point(427, 71);
+            three.Name = "three";
+            three.Size = new Size(166, 62);
+            three.TabIndex = 3;
+            three.Text = "2";
+            three.UseVisualStyleBackColor = false;
+            three.Click += three_Click;
+            // 
+            // four
+            // 
+            four.BackColor = SystemColors.HotTrack;
+            four.Location = new Point(622, 71);
+            four.Name = "four";
+            four.Size = new Size(166, 62);
+            four.TabIndex = 4;
+            four.Text = "3";
+            four.UseVisualStyleBackColor = false;
+            four.Click += four_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.HotTrack;
+            button5.Location = new Point(12, 139);
+            button5.Name = "button5";
+            button5.Size = new Size(189, 62);
+            button5.TabIndex = 5;
+            button5.Text = "4";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // six
+            // 
+            six.BackColor = SystemColors.HotTrack;
+            six.Location = new Point(216, 139);
+            six.Name = "six";
+            six.Size = new Size(166, 62);
+            six.TabIndex = 6;
+            six.Text = "5";
+            six.UseVisualStyleBackColor = false;
+            six.Click += six_Click;
+            // 
+            // seven
+            // 
+            seven.BackColor = SystemColors.HotTrack;
+            seven.Location = new Point(427, 139);
+            seven.Name = "seven";
+            seven.Size = new Size(166, 62);
+            seven.TabIndex = 7;
+            seven.Text = "6";
+            seven.UseVisualStyleBackColor = false;
+            seven.Click += seven_Click;
+            // 
+            // eight
+            // 
+            eight.BackColor = SystemColors.HotTrack;
+            eight.Location = new Point(622, 139);
+            eight.Name = "eight";
+            eight.Size = new Size(166, 62);
+            eight.TabIndex = 8;
+            eight.Text = "7";
+            eight.UseVisualStyleBackColor = false;
+            eight.Click += eight_Click;
+            // 
+            // nine
+            // 
+            nine.BackColor = SystemColors.HotTrack;
+            nine.Location = new Point(12, 207);
+            nine.Name = "nine";
+            nine.Size = new Size(189, 62);
+            nine.TabIndex = 9;
+            nine.Text = "8";
+            nine.UseVisualStyleBackColor = false;
+            nine.Click += nine_Click;
+            // 
+            // ten
+            // 
+            ten.BackColor = SystemColors.HotTrack;
+            ten.Location = new Point(216, 207);
+            ten.Name = "ten";
+            ten.Size = new Size(166, 62);
+            ten.TabIndex = 10;
+            ten.Text = "9";
+            ten.UseVisualStyleBackColor = false;
+            ten.Click += ten_Click;
+            // 
+            // eleven
+            // 
+            eleven.BackColor = SystemColors.HotTrack;
+            eleven.Location = new Point(427, 207);
+            eleven.Name = "eleven";
+            eleven.Size = new Size(166, 62);
+            eleven.TabIndex = 11;
+            eleven.Text = "/";
+            eleven.UseVisualStyleBackColor = false;
+            eleven.Click += eleven_Click;
+            // 
+            // twelve
+            // 
+            twelve.BackColor = SystemColors.HotTrack;
+            twelve.Location = new Point(622, 207);
+            twelve.Name = "twelve";
+            twelve.Size = new Size(166, 62);
+            twelve.TabIndex = 12;
+            twelve.Text = "*";
+            twelve.UseVisualStyleBackColor = false;
+            twelve.Click += twelve_Click;
+            // 
+            // thirteen
+            // 
+            thirteen.BackColor = SystemColors.HotTrack;
+            thirteen.Location = new Point(12, 275);
+            thirteen.Name = "thirteen";
+            thirteen.Size = new Size(189, 62);
+            thirteen.TabIndex = 13;
+            thirteen.Text = "-";
+            thirteen.UseVisualStyleBackColor = false;
+            thirteen.Click += thirteen_Click;
+            // 
+            // fourteen
+            // 
+            fourteen.BackColor = SystemColors.HotTrack;
+            fourteen.Location = new Point(216, 275);
+            fourteen.Name = "fourteen";
+            fourteen.Size = new Size(166, 62);
+            fourteen.TabIndex = 14;
+            fourteen.Text = "+";
+            fourteen.UseVisualStyleBackColor = false;
+            fourteen.Click += fourteen_Click;
+            // 
+            // fifteen
+            // 
+            fifteen.BackColor = SystemColors.HotTrack;
+            fifteen.Location = new Point(427, 275);
+            fifteen.Name = "fifteen";
+            fifteen.Size = new Size(166, 62);
+            fifteen.TabIndex = 15;
+            fifteen.Text = "C";
+            fifteen.UseVisualStyleBackColor = false;
+            fifteen.Click += fifteen_Click;
+            // 
+            // sixteen
+            // 
+            sixteen.BackColor = SystemColors.HotTrack;
+            sixteen.Location = new Point(622, 275);
+            sixteen.Name = "sixteen";
+            sixteen.Size = new Size(166, 62);
+            sixteen.TabIndex = 16;
+            sixteen.Text = ".";
+            sixteen.UseVisualStyleBackColor = false;
+            sixteen.Click += sixteen_Click;
+            // 
+            // button17
+            // 
+            button17.BackColor = Color.FromArgb(0, 0, 192);
+            button17.Location = new Point(12, 353);
+            button17.Name = "button17";
+            button17.Size = new Size(776, 85);
+            button17.TabIndex = 17;
+            button17.Text = "=";
+            button17.UseVisualStyleBackColor = false;
+            button17.Click += button17_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button17);
+            Controls.Add(sixteen);
+            Controls.Add(fifteen);
+            Controls.Add(fourteen);
+            Controls.Add(thirteen);
+            Controls.Add(twelve);
+            Controls.Add(eleven);
+            Controls.Add(ten);
+            Controls.Add(nine);
+            Controls.Add(eight);
+            Controls.Add(seven);
+            Controls.Add(six);
+            Controls.Add(button5);
+            Controls.Add(four);
+            Controls.Add(three);
+            Controls.Add(two);
+            Controls.Add(one);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button one;
+        private Button two;
+        private Button three;
+        private Button four;
+        private Button button5;
+        private Button six;
+        private Button seven;
+        private Button eight;
+        private Button nine;
+        private Button ten;
+        private Button eleven;
+        private Button twelve;
+        private Button thirteen;
+        private Button fourteen;
+        private Button fifteen;
+        private Button sixteen;
+        private Button button17;
+    }
+}
